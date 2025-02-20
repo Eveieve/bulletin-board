768527461ecd3cf686f8dfb566168532e1d55531
Eveieve <jhkdev1@gmail.com>
2025-02-20 19:57:47 +0900

    feat: stop running program when invalid input

--- main/src/Main.java
+++ main/src/Main.java
@@ -3,18 +3,27 @@ public class Main {
     public static void main(String[] args) {
 
 BoardManager manager = new BoardManager();
-int userInput = manager.getMenuInput();
 
-if(userInput == 1) {
-    System.out.println("You have chosen menu 1.");
-    manager.create();
+boolean status = true;
+while(status) {
+    int userInput = manager.getMenuInput();
 
-} else if(userInput ==2){
-    manager.menu2_read();
-    int subMenu = manager.getSubmenuOf2();
-    manager.submenuOf2(subMenu);
-} else if(userInput ==3) {
-    manager.menu3_clear();
+    if(userInput == 1) {
+        System.out.println("You have chosen menu 1.");
+        manager.create();
+
+    } else if(userInput ==2){
+        manager.menu2_read();
+        int subMenu = manager.getSubmenuOf2();
+        manager.submenuOf2(subMenu);
+    } else if(userInput ==3) {
+        manager.menu3_clear();
+    } else {
+        System.out.println("Invalid ouput! Program shutting down. ");
+        status = false;
+
+    } //////// 이외 값이나 잘못된 타입이 입력됐을때 예외 처리 필요
 }
+
     }
 }
