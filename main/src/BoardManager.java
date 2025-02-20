3c948eb720ab59bc3bee7be82102b7f2860f5cd2
Eveieve <jhkdev1@gmail.com>
2025-02-20 09:39:13 +0900

    feat: make Main methodo work

--- main/src/BoardManager.java
+++ main/src/BoardManager.java
@@ -8,6 +8,9 @@ public class BoardManager {
     // 게시물 목록을 담을 boards ArrayList.
     public List<Board> boards01 = new ArrayList<>();
     public Map<Integer, Board> boardMap = new HashMap<>();
+    ///////////// 처음 포스트 몇개 넣기!!
+
+
     // listBoards = 모든 게시물 목록을 출력한다.
     // int printMenu = 메뉴를 출력한다.
     // getMenuInput = 사용자의 값을 입력 받는다.
@@ -19,23 +22,7 @@ public class BoardManager {
         methodMap.put(1, this::create); // 패스할걸 여기서 미리 정해야 하나?
     }
 
-
-    // 메뉴 2번 - 특정 번호의 보드 출력
-    // bno 반환하기.
-    int printBoards(){
-        int bno = sc.nextInt();
-        for(Board board: boards01) {
-            if(board.getBno() == bno)
-            System.out.println(board);
-        }
-        return bno;
-    }
-
-    int getSubmenuOf2() {
-        System.out.println("Choose your submenu: 1. Update | 2. Delete | 3. List");
-        int subMenu = sc.nextInt();
-        return subMenu;
-    }
+    // 처음 사용자로부터 메뉴 입력 받기. 입력 정수 반환.
     int getMenuInput() {
         System.out.println("Main Menu: 1. Create | 2. Read | 3. Clear | 4.Exit ");
         System.out.println("Choose your option: ");
@@ -48,6 +35,7 @@ public class BoardManager {
     // option 1
     // 입력 받고 게시물 하나 생성하기.
     public void create() {
+
         // 보드 하나 생성하여 리스트에 순서대로 저장하기
         int bno = sc.nextInt();
         String writer = sc.nextLine();
@@ -56,10 +44,25 @@ public class BoardManager {
         Board board = new Board.BoardBuilder(bno, writer, content).build();
         boards01.add(board);
         System.out.println(boards01);
+
+    }
+
+    // 메뉴 2번 - 특정 번호의 보드 출력
+    // bno 반환하기.
+    int menu2_read(){
+        boardMap.forEach((key, content) -> System.out.println("Post number: " + key + "Content: " + content));
+    }
+
+    int getSubmenuOf2() {
+        System.out.println("Choose your submenu: 1. Update | 2. Delete | 3. List");
+        int subMenu = sc.nextInt();
+        return subMenu;
     }
 
     // 2번 보조 메뉴 - 1.내용, 작성자 업데이트
-    void submenuOf2(int subMenu, int bno) {
+    void submenuOf2(int subMenu) {
+        System.out.println("Enter your post number: ");
+        int bno = sc.nextInt();
         Board selectedBoard = boardMap.get(bno); // 사용자가 선택한 게시물 번호
 
         if(subMenu == 1) {
@@ -95,7 +98,7 @@ public class BoardManager {
     }
 
    // option 3
-    void option3Clear() {
+    void menu3_clear() {
         System.out.println("Are you sure you want to delete all posts?: 1. Yes | 2. No");
         int input = sc.nextInt();
         if(input == 1) {
--- main/src/Main.java
+++ main/src/Main.java
@@ -8,7 +8,18 @@ public class Main {
     public static void main(String[] args) {
 
 BoardManager manager = new BoardManager();
+int userInput = manager.getMenuInput();
 
+if(userInput == 1) {
+    manager.create();
+
+} else if(userInput ==2){
+    manager.menu2_read();
+    int subMenu = manager.getSubmenuOf2();
+    manager.submenuOf2(subMenu);
+}else if(userInput ==3) {
+    manager.menu3_clear();
+}
 
     }
 }
